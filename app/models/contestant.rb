eaf432483108096a3b56355d29da497053836689
Melanie Tran <melaniexx02@gmail.com>
2020-05-15 10:16:56 -0600

    Move average years test to contest model

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -3,4 +3,8 @@ class Contestant <ApplicationRecord
 
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def self.average_years_experience
+    average(:years_of_experience)
+  end
 end
--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "methods" do
+    it ".average_years_experience" do
+      challenge = Challenge.create(theme: "Disney", project_budget: "2000")
+      project1 = Project.create(name: "Mulan Warrior", material: "Silk", challenge_id: challenge.id)
+
+      moana = project1.contestants.create(name: "Moana", age: "18", hometown: "Athens", years_of_experience: 3)
+      ralphie = project1.contestants.create(name: "Ralphie", age: "34", hometown: "Brooklyn", years_of_experience: 10)
+
+      expect(Contestant.average_years_experience).to eq(6.5)
+    end
+  end
 end

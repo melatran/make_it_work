49c1a81d73688d03cf7f135b0c878154bab1c306
Melanie Tran <melaniexx02@gmail.com>
2020-05-15 10:05:02 -0600

    Created test for total_contestant method in project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "methods" do
+    it "can .total_contestants" do
+    challenge = Challenge.create(theme: "Disney", project_budget: "2000")
+    project1 = Project.create(name: "Mulan Warrior", material: "Silk", challenge_id: challenge.id)
+
+    moana = project1.contestants.create(name: "Moana", age: "18", hometown: "Athens", years_of_experience: "3")
+    ralphie = project1.contestants.create(name: "Ralphie", age: "34", hometown: "Brooklyn", years_of_experience: "10")
+
+    expect(project1.total_contestants).to eq(2)
+    end
+  end
 end
